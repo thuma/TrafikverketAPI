367229ab98fe262c87881a7e12c5378e84048354
Martin Thuresson <martin.thure@gmail.com>
2013-11-20 17:57:56 +0100

    modifed for trafikverket.

--- addwgs84.php
+++ addwgs84.php
@@ -1,21 +1,20 @@
 <?php
-//require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/RT90Position.php';
-//require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/SWEREF99Position.php';
-//require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/WGS84Position.php';
+require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/RT90Position.php';
+require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/SWEREF99Position.php';
+require_once dirname(__FILE__) . '/CoordinateTransformationLibrary/src/coordinatetransformation/positions/WGS84Position.php';
 
 $all = json_decode(file_get_contents('all-sweref.json'));
-print_r($all);
-
+$all = $all->Stations
 die();
 foreach($all as $key => $station)
 	{
-		if(isset($all->{$key}->position->wgs84) == FALSE)
+		if(isset($all[$key]->position->wgs84) == FALSE)
 		  {
-		  $position = new RT90Position($all->{$key}->position->rt90->x, $all->{$key}->position->rt90->y);
+		  $position = new SWEREF99Position($station->NS,$station->EW);
 		  $wgs84 = $position->toWGS84();
-		  $all->{$key}->position->wgs84 = new stdClass;
-		  $all->{$key}->position->wgs84->lat = $wgs84->getLatitude();
-		  $all->{$key}->position->wgs84->long = $wgs84->getLongitude();
+		  $all[$key]->position->wgs84 = new stdClass;
+		  $all[$key]->position->wgs84->lat = $wgs84->getLatitude();
+		  $all[$key]->position->wgs84->long = $wgs84->getLongitude();
 		  }
 	}
 file_put_contents('coord-wgs84.json',json_encode($all));
