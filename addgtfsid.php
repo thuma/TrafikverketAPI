6224533fa792d7a330b171ae0baa367d114e2edc
Martin Thuresson <martin.thure@gmail.com>
2013-11-24 20:35:24 +0100

    Rerunn with less missing stops. Still some missing.

--- addgtfsid.php
+++ addgtfsid.php
@@ -7,13 +7,18 @@ foreach($all as $key => $station)
 	{
 		if(isset($all[$key]->gtfs) == FALSE OR $all[$key]->gtfs->id == NULL){
 			print "\n".$all[$key]->name;
-			$stop = getClosestStation(floatval($all[$key]->position->wgs84->lat),floatval($all[$key]->position->wgs84->long));
+			$stop = getClosestStation(floatval($all[$key]->position->wgs84->lat),floatval($all[$key]->position->wgs84->long),1.0,7402000);
+			if($stop->stop_name == "not found")
+				{
+				$stop = getClosestStation(floatval($all[$key]->position->wgs84->lat),floatval($all[$key]->position->wgs84->long),100);
+				}
 			$all[$key]->gtfs = new stdClass;
 			$all[$key]->gtfs->id = $stop->stop_id;
 			$all[$key]->gtfs->name = $stop->stop_name;
 			$all[$key]->gtfs->lat = $stop->stop_lat;
 			$all[$key]->gtfs->long = $stop->stop_lon;
-			print $stop->stop_name;
+			$all[$key]->gtfs->offsett = $stop->distance;
+			print $station->Namn .';'.$stop->stop_name.';'.$stop->stop_id.';'.$stop->distance;
 		}
 	}
 file_put_contents('coord-gtfs.json',json_encode($all));
